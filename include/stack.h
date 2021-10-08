28dd4e3cbf1ab408948b80a3b9cd6007112c5af6
jbbAU <78474042+jbbAU@users.noreply.github.com>
2021-10-08 15:51:02 +0200

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -1,8 +1,8 @@
 #ifndef __STACK_H_
 #define __STACK_H_
-
-#include <stdlib.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct node
 {
@@ -10,12 +10,15 @@ typedef struct node
     struct node* next;
 } node;
 
-void initialize(node**);
-void push(int, node**);
-int pop(node**);
-bool full(node** ptr);
-bool empty(node**);
 
+typedef struct{
+  node* head;
+} stack;
+
+void initialize(stack* );
+void push(int, stack* );
+int pop(stack*);
+bool empty(stack*);
+bool full(stack*);
 
 #endif // __STACK_H_
-
