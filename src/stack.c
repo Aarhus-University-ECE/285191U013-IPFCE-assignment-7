f67cc64f5d10753b28952ea6dd1263b3f33408c2
jbbAU <78474042+jbbAU@users.noreply.github.com>
2021-10-08 15:38:33 +0200

    Update stack.c

--- src/stack.c
+++ src/stack.c
@@ -1,24 +1,24 @@
 #include "stack.h"
-void initialize(node_t** ptr)
+void initialize(node** ptr)
 {
     // Add your code here
 }
 
-void push(int x, node_t** ptr){
+void push(int x, node** ptr){
     // Add your code here
 }
 
-int pop(node_t** ptr){
-    // Add your code here
-    return -1;
-}
-
-bool full(node_t** ptr){
+int pop(node** ptr){
     // Add your code here
     return -1;
 }
 
-bool empty(node_t** ptr){
+bool full(node** ptr){
+    // Add your code here
+    return -1;
+}
+
+bool empty(node** ptr){
     // Add your code here   
     return -1;
 }
