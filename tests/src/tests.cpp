baf6069b05eee9ade3b6808fecac9bc0e743a491
au614630 <au614630@uni.au.dk>
2021-10-11 16:29:31 +0200

    Added taylor_sine tests

--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -11,7 +11,15 @@ extern "C"
 
 TEST_CASE("taylor_sine")
 {
-    // Jens 
+    REQUIRE_THAT(taylor_sine(3.1415, 4),
+        Catch::Matchers::WithinRel(0.000093, 0.05));
+
+    REQUIRE_THAT(taylor_sine(1.57075, 10),
+        Catch::Matchers::WithinRel(0.999944, 0.05));
+
+    REQUIRE_THAT(taylor_sine(0, 1),
+        Catch::Matchers::WithinRel(0, 0.05));
+    
 }
 
 TEST_CASE("stack"){
