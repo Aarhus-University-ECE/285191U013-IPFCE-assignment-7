52783176d7415758f4b17d42ad496fa9aff302c1
kpbaks <kristoffer.pbs@tuta.io>
2023-10-08 16:35:10 +0200

    feat: finish updating tests

--- stack.h
+++ stack.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 typedef struct node {
   int data;
--- taylor_sine.h
+++ taylor_sine.h
@@ -1,3 +1,4 @@
 #pragma once
 
 // Add header defintion
+double taylor_sine(double x, int n);
